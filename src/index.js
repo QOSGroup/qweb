190adc3649bf7e8184aeebc6cfc1175c1470d943
Peter Hu <yesky93@sina.com>
2018-12-20 07:05:14 +0800

    add test tools & account api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import Trx from './model/trx'
+import Account from './model/account'
 import DefaultConfig from './config'
+import HTTPEndpoint from './util/HTTPEndpoint'
 
 export default class QOSWeb {
-	constructor(config) {
-		this.config = config || DefaultConfig
+	constructor(config = DefaultConfig) {
+		this.config = config
+		this.HTTPEndpoint = new HTTPEndpoint(config)
 	}
 
 	// setConfig(config) {}
 
-	// getBalance(address) {}
+	isValidAccount(address){
+		console.log(address)
+		return true
+	}
+
+	async getAccount(address) {
+		const result = await this.HTTPEndpoint.requestGetAccount(address)
+		console.log(result)
+		const account = new Account()
+		account.address = address
+		return account
+	}
 
 	// getTransaction(transactionID) {
 	// 	const trx = new Trx()
