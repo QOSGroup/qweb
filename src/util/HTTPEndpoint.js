3910e56114b1122589494283b6357cbe40dc5fee
Peter Hu <yesky93@sina.com>
2018-12-20 07:05:14 +0800

    add test tools & account api

--- src/util/HTTPEndpoint.js
+++ src/util/HTTPEndpoint.js
@@ -2,21 +2,14 @@ import axios from 'axios'
 
 const createAxioRequest = (baseURL, timeout)=>{
 	return axios.create({
-		baseURL: `${this.HTTPBaseURL}/${this.chainId}`,
+		baseURL: baseURL,
 		timeout: timeout
 	})
 }
 
 export default class HTTPEndpoint {
 	constructor(config){
-		this.request = null
-		this.HTTPBaseURL = null
-		this.chainId = null
-		this.init(config)
-	}
-
-	init(config){
-		this.HTTPBaseURL = config.HTTPAPIEndpoint
+		this.HTTPBaseURL = config.HTTPAPINode
 		this.chainId = config.chainId
 		this.request = createAxioRequest(`${this.HTTPBaseURL}/${this.chainId}`, 30000)
 	}
@@ -31,7 +24,7 @@ export default class HTTPEndpoint {
 		this.request = createAxioRequest(`${this.HTTPBaseURL}/${this.chainId}`, 30000)
 	}
 
-	requestGetBalance(address){
+	requestGetAccount(address){
 		return this.request({
 			url: `/accounts/${address}`
 		})
