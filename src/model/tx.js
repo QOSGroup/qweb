eb79007a2530f118003500dfd106e5550f94aa6f
文顺 章 <378253487@qq.com>
2018-12-28 20:07:00 +0800

    完善签名方法，后续继续开发tx执行

--- src/model/tx.js
+++ src/model/tx.js
@@ -175,7 +175,7 @@ export default class Tx {
 			this.oneToMany(privateKey)
 		}
 		const keyPair = this._qweb.recoveryAccountByPrivateKey(privateKey).keyPair
-		console.log('keyPair',keyPair)
+		console.log('keyPair', keyPair)
 		console.log(tool.decodeBase64(privateKey))
 
 		const chainId_hex = tool.stringToHex(this.tx.chainid)
@@ -192,12 +192,16 @@ export default class Tx {
 		const nonce_32_str = nonce_str.slice(-32),
 			nonce_16_str = nonce_str.slice(-16)
 
-		const signature_str = this.tx.signatureArr.join(this[getSep]()) + nonce_32_str + chainId_hex + nonce_16_str
+		const signature_str = this.tx.signatureArr.join(this[getSep]()) + nonce_32_str + chainId_hex //+ nonce_16_str
 		console.log(signature_str)
 		const signature_buffer = Buffer.from(signature_str, 'hex')
 		const signatureData = nacl.sign.detached(signature_buffer, keyPair.secretKey)
 		console.log(signatureData)
 		console.log(tool.buf2hex(signatureData.buffer))
+		console.log(tool.encodeBase64(signatureData))
+
+
+		console.log(tool.decodeBase64('GwJwoChg7SE19bIBucL3yM0STkqb1RmxfIf2a7AKODG+0WckvyDDL2BIiCLewlA7aDQElZsg/ihxYhmR1q+PDw=='))
 
 		const pubKeyEd25519 = new PubKeyEd25519(keyPair.publicKey)
 		const signature = new Signature(pubKeyEd25519, tool.encodeBase64(signatureData), '7')
@@ -252,4 +256,53 @@ export default class Tx {
 		// const pubKeyEd25519 = new PubKeyEd25519(this.trx.publicKey)
 		// const sender = new Sender(this.sender, 2, 0)
 	}
+}
+
+
+if (!window.atob) {
+	var tableStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	var table = tableStr.split('')
+
+	window.atob = function (base64) {
+		if (/(=[^=]+|={3,})$/.test(base64)) throw new Error('String contains an invalid character')
+		base64 = base64.replace(/=/g, '')
+		var n = base64.length & 3
+		if (n === 1) throw new Error('String contains an invalid character')
+		for (var i = 0, j = 0, len = base64.length / 4, bin = []; i < len; ++i) {
+			var a = tableStr.indexOf(base64[j++] || 'A'), b = tableStr.indexOf(base64[j++] || 'A')
+			var c = tableStr.indexOf(base64[j++] || 'A'), d = tableStr.indexOf(base64[j++] || 'A')
+			if ((a | b | c | d) < 0) throw new Error('String contains an invalid character')
+			bin[bin.length] = ((a << 2) | (b >> 4)) & 255
+			bin[bin.length] = ((b << 4) | (c >> 2)) & 255
+			bin[bin.length] = ((c << 6) | d) & 255
+		}
+		return String.fromCharCode.apply(null, bin).substr(0, bin.length + n - 4)
+	}
+
+	window.btoa = function (bin) {
+		for (var i = 0, j = 0, len = bin.length / 3, base64 = []; i < len; ++i) {
+			var a = bin.charCodeAt(j++), b = bin.charCodeAt(j++), c = bin.charCodeAt(j++)
+			if ((a | b | c) > 255) throw new Error('String contains an invalid character')
+			base64[base64.length] = table[a >> 2] + table[((a << 4) & 63) | (b >> 4)] +
+				(isNaN(b) ? '=' : table[((b << 2) & 63) | (c >> 6)]) +
+				(isNaN(b + c) ? '=' : table[c & 63])
+		}
+		return base64.join('')
+	}
+
+}
+
+function hexToBase64(str) {
+	return btoa(String.fromCharCode.apply(null,
+		str.replace(/\r|\n/g, '').replace(/([\da-fA-F]{2}) ?/g, '0x$1 ').replace(/ +$/, '').split(' '))
+	)
+}
+
+function base64ToHex(str) {
+	for (var i = 0, bin = atob(str.replace(/[ \r\n]+$/, '')), hex = []; i < bin.length; ++i) {
+		var tmp = bin.charCodeAt(i).toString(16)
+		if (tmp.length === 1) tmp = '0' + tmp
+		hex[hex.length] = tmp
+	}
+	return hex.join(' ')
 }
