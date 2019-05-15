82260fc4d8dbb9329678a0472901d06f14e11bb5
huming0618 <huming0618@gmail.com>
2019-05-15 14:55:45 +0800

    remove quote

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -17,7 +17,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				include: [resolve('src'), resolve('test'), resolve('node_modules/js-amino'),],
+				include: [resolve('src'), resolve('test'), resolve('node_modules/js-amino')],
 			},
 			{
 				test: /[\\\/]tweetnacl[\\\/]/,
